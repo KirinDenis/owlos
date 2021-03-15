ce64882065db82f67c20f21cbe6546920130c089
kirindenis1977@gmail.com <kirindenis1977@gmail.com>
2021-03-15 17:18:24 +0200

    first model

---
+++ OWLOSEcosystem/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OWLOSEcosystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GoOpenGL_Click(object sender, RoutedEventArgs e)
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Title = "LearnOpenTK - Creating a Window",
+            };
+
+            // To create a new window, create a class that extends GameWindow, then call Run() on it.
+            using (var window = new OpenTKWindow(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
