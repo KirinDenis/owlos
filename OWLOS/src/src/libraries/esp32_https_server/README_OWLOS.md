1e650cf43ab239a97a6aee7783380bf675175c6b
kirindenis1977@gmail.com <kirindenis1977@gmail.com>
2021-04-21 17:52:05 +0300

    fixes for building on ESP8266

--- OWLOS/src/src/libraries/esp32_https_server/README_OWLOS.md
+++
@@ -1,2 +0,0 @@
-We had to modify the original ESP32 HTTPS Server source code. The fact is that OWLOS should be able to change the default HTTP port number.
-SEE: HTTPSServer.hpp, HTTPServer.hpp
--- OWLOS/src/src/libraries/esp32_https_server/docs/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['beginqueryparameters',['beginQueryParameters',['../classhttpsserver_1_1ResourceParameters.html#a05eebf06e0b2fd9a19299c18d322577a',1,'httpsserver::ResourceParameters']]]
-];
