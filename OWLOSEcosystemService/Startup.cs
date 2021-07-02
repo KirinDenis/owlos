5089da2b46611b9e727a21b4dd999eb64b997002
Bob Pavlov <hiroyashy@gmail.com>
2021-07-02 22:09:06 +0300

    artic

--- OWLOSEcosystemService/Startup.cs
+++ OWLOSEcosystemService/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OWLOSEcosystemService.Data;
 using OWLOSEcosystemService.Services.Things;
-
+//artic
 namespace OWLOSEcosystemService
 {
     public class Startup
